150c2c6ed81d91adffb0b0a8a7eccfa34b6082f8
Sam Martin <me@smartin.io>
2023-04-14 01:27:23 +0200

    Add drag & drop

--- views/MyHand.tsx
+++ views/MyHand.tsx
@@ -1,4 +1,15 @@
 import { Box, Button, Flex, HStack, SlideFade, useDisclosure } from '@chakra-ui/react'
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverEvent,
+  PointerSensor,
+  useDraggable,
+  useDroppable,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core'
+import { CSS } from '@dnd-kit/utilities'
 import { useEffect, useRef, useState } from 'react'
 import { Card, Errors } from 'shared/types'
 import { useSnapshot } from 'store'
@@ -11,6 +22,7 @@ import { PlayingCard } from 'views/PlayingCard'
 export const MyHand = () => {
   const { snapshot } = useSnapshot()
   const [selectedCard, setSelectedCard] = useState<Card>()
+  const [pendingDropCard, setPendingDropCard] = useState<Card>()
   const [minHeight, setMinHeight] = useState(0)
   const [shouldFadeIn, setShouldFadeIn] = useState(false)
   const [maxHeight, setMaxHeight] = useState<number>()
@@ -20,6 +32,9 @@ export const MyHand = () => {
   const mutationOptions = { onSuccess: dataHandler(() => {}, handleError) }
   const playBindingTrickMutation = trpc.playBindingTrick.useMutation(mutationOptions)
   const playRegularTrickMutation = trpc.playRegularTrick.useMutation(mutationOptions)
+
+  const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 1 } })
+  const sensors = useSensors(pointerSensor)
 
   useEffect(() => {
     switch (snapshot?.roundPhase) {
@@ -35,7 +50,7 @@ export const MyHand = () => {
     if (clientHeight !== minHeight && snapshot?.roundPhase === 'bidding') {
       setMinHeight(clientHeight)
     }
-  })
+  }, [minHeight, snapshot?.roundPhase])
 
   useEffect(() => {
     let timeout = setTimeout(() => {
@@ -55,14 +70,23 @@ export const MyHand = () => {
   const { gameId, playerSecret } = snapshot
 
   const getCardStyle = (cardId: string) => {
-    const transition = 'all 0.2s ease'
-    const userSelect = 'none'
+    const transition = 'all 0.3s ease'
+    const userSelect = 'none' as const
+
+    if (pendingDropCard) {
+      const pending = pendingDropCard.id === cardId
+      return {
+        userSelect,
+        transition,
+        transform: pending ? undefined : 'translateY(100px)',
+      }
+    }
 
     if (selectedCard?.id !== cardId) {
       return {
         transition,
         userSelect,
-      } as const
+      }
     }
 
     return {
@@ -70,7 +94,7 @@ export const MyHand = () => {
       userSelect,
       boxShadow: '0px 5px 15px rgba(0,0,0,0.2)',
       transform: 'translateY(-60px)',
-    } as const
+    }
   }
 
   const getButtonStyle = (cardId: string) => {
@@ -107,9 +131,9 @@ export const MyHand = () => {
     }
   }
 
-  const playRegularTrick = () => {
-    if (selectedCard) {
-      playRegularTrickMutation.mutate({ gameId, playerSecret, card: selectedCard })
+  const playRegularTrick = (card = selectedCard) => {
+    if (card) {
+      playRegularTrickMutation.mutate({ gameId, playerSecret, card })
     }
   }
 
@@ -121,52 +145,76 @@ export const MyHand = () => {
     }
   }
 
+  const handleDragEnd = (e: DragEndEvent) => {
+    setPendingDropCard(undefined)
+    setSelectedCard(undefined)
+    if (!e.over || !isMyTurn) {
+      return
+    } else if (!snapshot.trumpSuit) {
+      modal.onOpen()
+    } else {
+      playRegularTrick(e.active.data.current as Card)
+    }
+  }
+
+  const handleDragOver = (e: DragOverEvent) => {
+    if (isMyTurn) {
+      setSelectedCard(undefined)
+      setPendingDropCard(e.over ? (e.active.data.current as Card) : undefined)
+    }
+  }
+
   const { isMyTurn, myCards, roundPhase } = snapshot
   const sortedCards = sortBySuitAndValue(myCards)
   const canPlay = isMyTurn && roundPhase === 'tricking'
 
   return (
-    <Box minHeight={minHeight} maxHeight={maxHeight}>
-      <Box position="fixed" bottom={0} left={0} right={0} ref={ref}>
-        <SlideFade in={shouldFadeIn} offsetY="120px">
-          <Box paddingX="5" marginBottom="-14">
-            <HStack spacing="-20" opacity={canPlay ? 1 : 0.3}>
-              {sortedCards.map((card) => (
-                <Flex key={card.id} direction="column" alignItems="center">
-                  <Box paddingBottom="5">
-                    <Button
-                      style={getButtonStyle(card.id)}
-                      onClick={handlePlayPress}
-                      variant="solid"
-                      colorScheme="green"
-                      borderRadius="3xl"
-                      disabled={!canPlay}
-                      zIndex={card.id === selectedCard?.id ? undefined : -999}
-                    >
-                      Play
-                    </Button>
-                  </Box>
-                  <Box maxHeight={200}>
-                    <PlayingCard
-                      style={getCardStyle(card.id)}
-                      onClick={() => handleCardClick(card)}
-                      card={card}
-                    />
-                  </Box>
-                </Flex>
-              ))}
-            </HStack>
-          </Box>
-        </SlideFade>
-      </Box>
+    <DndContext onDragEnd={handleDragEnd} onDragOver={handleDragOver} sensors={sensors}>
+      <Droppable id="dropzone" enabled={roundPhase === 'tricking'} />
+      <Box minHeight={minHeight} maxHeight={maxHeight}>
+        <Box position="fixed" bottom={0} left={0} right={0} ref={ref}>
+          <SlideFade in={shouldFadeIn} offsetY="120px">
+            <Box paddingX="5" marginBottom="-14">
+              <HStack spacing="-20" opacity={canPlay ? 1 : 0.3}>
+                {sortedCards.map((card) => (
+                  <Flex key={card.id} direction="column" alignItems="center">
+                    <Box paddingBottom="5">
+                      <Button
+                        style={getButtonStyle(card.id)}
+                        onClick={handlePlayPress}
+                        variant="solid"
+                        colorScheme="green"
+                        borderRadius="3xl"
+                        disabled={!canPlay}
+                        zIndex={card.id === selectedCard?.id ? undefined : -999}
+                      >
+                        Play
+                      </Button>
+                    </Box>
+                    <Draggable id={card.id} card={card}>
+                      <Box maxHeight={200}>
+                        <PlayingCard
+                          style={getCardStyle(card.id)}
+                          onClick={() => handleCardClick(card)}
+                          card={card}
+                        />
+                      </Box>
+                    </Draggable>
+                  </Flex>
+                ))}
+              </HStack>
+            </Box>
+          </SlideFade>
+        </Box>
 
-      <ChoosePartnerModal
-        selectedCard={selectedCard}
-        onClose={modal.onClose}
-        onChoose={playBindingTrick}
-        isOpen={modal.isOpen}
-      />
-    </Box>
+        <ChoosePartnerModal
+          selectedCard={selectedCard}
+          onClose={modal.onClose}
+          onChoose={playBindingTrick}
+          isOpen={modal.isOpen}
+        />
+      </Box>
+    </DndContext>
   )
 }
 
@@ -186,4 +234,44 @@ const handleError = (error?: Errors) => {
       break
     }
   }
+}
+
+const Draggable = ({
+  id,
+  card,
+  children,
+}: {
+  id: string
+  card: Card
+  children: React.ReactNode
+}) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({ id, data: card })
+
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  }
+
+  return (
+    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {children}
+    </button>
+  )
+}
+
+const Droppable = ({ id, enabled }: { id: string; enabled: boolean }) => {
+  const { setNodeRef } = useDroppable({ id, disabled: !enabled })
+
+  return (
+    <Box
+      zIndex={enabled ? undefined : -999}
+      ref={setNodeRef}
+      id={id}
+      position="absolute"
+      width="100%"
+      height="70%"
+      bottom={200}
+      left={0}
+      right={0}
+    />
+  )
 }
